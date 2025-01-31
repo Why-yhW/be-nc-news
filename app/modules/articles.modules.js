6868fffdb7530ee18eee47a37a3c9d20a12d4c51
James Barlow <jab.2002@hotmail.co.uk>
2025-01-31 02:20:22 +0000

    added the comment_count to the SQL query and have tested it. Also edited the endpoint.json to show this change.

--- app/modules/articles.modules.js
+++ app/modules/articles.modules.js
@@ -66,10 +66,19 @@ exports.fetchArticles = ({ sorted_by, order, topic }) => {
 
 exports.fetchArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE articles.article_id = $1`, [
-      article_id,
-    ])
+    .query(
+      `SELECT 
+        articles.*, 
+        COUNT(comments.article_id) ::INT AS comment_count
+      FROM articles
+      LEFT JOIN comments 
+      ON comments.article_id = articles.article_id 
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id`,
+      [article_id]
+    )
     .then(({ rows }) => {
+      console.log(rows);
       if (rows.length === 0) {
         return Promise.reject({ message: "Article not found" });
       } else {
